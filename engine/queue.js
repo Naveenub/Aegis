95330cfca5d5ae3bb4181c8552ba1247db08659c
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-05-06 17:18:41 +0530

    Add QueueEvents and remove unused queue instance

--- engine/queue.js
+++ engine/queue.js
@@ -1,10 +1,7 @@
-import { Queue } from 'bullmq';
+import { Queue, QueueEvents } from 'bullmq';
 import IORedis from 'ioredis';
 
 const connection = new IORedis();
 
 export const taskQueue = new Queue('aegis-tasks', { connection });
-
-export const queue = new Queue('tasks', {
-  connection: { host: '127.0.0.1', port: 6379 }
-});
+export const queueEvents = new QueueEvents('aegis-tasks', { connection });
