152bcb0b3a1ce552481b926801adb3356ed4cbb9
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-05-06 16:59:15 +0530

    Initialize IORedis connection for task queue
    Add IORedis connection for task queue.

--- engine/queue.js
+++ engine/queue.js
@@ -1,4 +1,9 @@
 import { Queue } from 'bullmq';
+import IORedis from 'ioredis';
+
+const connection = new IORedis();
+
+export const taskQueue = new Queue('aegis-tasks', { connection });
 
 export const queue = new Queue('tasks', {
   connection: { host: '127.0.0.1', port: 6379 }
